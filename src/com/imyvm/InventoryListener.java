e4492f929000eafdc0f839b34e0c4f364f7d7412
huangwang1003 <huangwang1003@gmail.com>
2018-03-11 18:32:02 +0800

    Add InventoryListener

---
+++ src/com/imyvm/InventoryListener.java
@@ -0,0 +1,33 @@
+package com.imyvm;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryListener implements Listener {
+
+    ItemMail itemMail;
+
+    public InventoryListener(ItemMail it){
+        it = itemMail;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void onInventoryClick(InventoryClickEvent event) {
+
+        if (!(event.getWhoClicked() instanceof Player)) {
+            return;
+        }
+        Player p = (Player) event.getWhoClicked();
+        if (event.getInventory().getTitle().equalsIgnoreCase("ItemMail for imyvm")) {
+            event.setCancelled(true);
+            p.updateInventory();
+            if (event.getRawSlot() == 0) {
+                p.closeInventory();
+                p.openInventory(event.getInventory());
+            }
+        }
+    }
+}
