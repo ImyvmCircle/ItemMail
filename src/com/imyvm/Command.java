7b697b5ba5d9c41039c5808cbab46cd4d9dcacf1
huangwang1003 <huangwang1003@gmail.com>
2018-03-11 17:43:36 +0800

    Update Commands, which could check the inventory is really full mostly.

--- src/com/imyvm/Command.java
+++ src/com/imyvm/Command.java
@@ -11,13 +11,14 @@ import org.bukkit.inventory.ItemStack;
 import static com.imyvm.ItemMail.econ;
 
 import java.text.DecimalFormat;
+import java.util.*;
 
 public class Command implements CommandExecutor {
 
-    ItemMail itemMail;
-    public Command(ItemMail it){
-        it = itemMail;
-    }
+//    ItemMail itemMail;
+//    public Command(ItemMail it){
+//        it = itemMail;
+//    }
     private static int slots = ItemMail.getSlots();
     private static double price = ItemMail.getPrice();
     private String null_mainhand = ItemMail.getMessage_null_mainhand();
@@ -60,13 +61,15 @@ public class Command implements CommandExecutor {
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&',this.null_inv));
                         return false;
                     }
-                    ItemStack[] items = inventory.getContents();
+                    ItemStack[] mid = playerinv.getStorageContents();
 
-                    if((playerinv.getStorageContents().length - getAmount(playerinv)) >= getAmount(inventory)){
-                        for (ItemStack it:items){
-                            if (it != null)
-                                playerinv.addItem(it);
-                        }
+                    Inventory midv = Bukkit.createInventory(null, slots, player.getName());
+                    midv.setStorageContents(mid);
+
+                    ItemStack[] items = inventory.getStorageContents();
+
+                    if (realAdd(midv, items)){
+                        playerinv.addItem(items);
                         player.updateInventory();
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b提取成功"));
                         Inventory inv1 = Bukkit.createInventory(null, slots, player.getName());
@@ -190,81 +193,107 @@ public class Command implements CommandExecutor {
         return true;
     }
 
-    private boolean sendtoal(Player player){
-        DecimalFormat df = new DecimalFormat( "0.00 ");
-        Inventory playerinv = player.getInventory();
-        Inventory inventory = SQLActions.loaddata(player);
-        int inv_amount = inventory.getSize();
-        if (empty(playerinv.getStorageContents())){
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',this.null_inventory));
-            return false;
-        }
-        if((inventory.getSize() - getAmount(inventory)) >= getAmount(playerinv)){
-            Double totalprice = price*getamount(playerinv);
-            int amount=0;
-            if (econ.has(player, totalprice)){
-                for (int i = 0; i < playerinv.getStorageContents().length; i++) {
-                    ItemStack it = playerinv.getItem(i);
-                    if (it != null){
-                        inventory.addItem(it);
-                        playerinv.setItem(i,new ItemStack(Material.AIR));
-                        amount += it.getAmount();
-                    }
+    private boolean sendtoal(Player player) {
+        DecimalFormat df = new DecimalFormat("0.00 ");
+        if (SQLActions.playerDataContainsPlayer(player.getUniqueId())) {
+            Inventory inv_s = SQLActions.loaddata(player);
+            ItemStack[] itemStack = player.getInventory().getStorageContents();
 
-                }
-                player.updateInventory();
-                SQLActions.uploaddata(player, inventory);
-                //Double totalprice = price*amount;
-                econ.withdrawPlayer(player, totalprice);
-                //等待给某op
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                        "&b投递成功，花费 &6"+df.format(totalprice)+"&6D"));
-            }else {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b你没有足够的钱！"));
+            Inventory midv = Bukkit.createInventory(null, slots, player.getName());
+            midv.setStorageContents(inv_s.getStorageContents());
+
+            if (empty(itemStack)) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', this.null_inventory));
                 return false;
             }
-        }else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    this.no_slots+getAmount(inventory)+"/"+inv_amount));
-            return false;
-        }
-        return true;
-    }
-
-    private boolean sendsingle(Player player, Player self){
-        DecimalFormat df = new DecimalFormat( "0.00 ");
-        if (SQLActions.playerDataContainsPlayer(player.getUniqueId())){
-            Inventory inv_s = SQLActions.loaddata(player);
-            if (fullinventory(inv_s)) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',this.full_inv));;
-                return false;
-            } else {
-                ItemStack itemStack = self.getInventory().getItemInMainHand();
-
-                if (itemStack != null && !itemStack.getType().equals(Material.AIR)){
-                    if (econ.has(self, price)){
-                        inv_s.addItem(itemStack);
-                        SQLActions.uploaddata(player, inv_s);
-                        player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-                        player.updateInventory();
-                        econ.withdrawPlayer(self, price*itemStack.getAmount());
-                        // 等待给某op
-                        self.sendMessage(ChatColor.translateAlternateColorCodes('&',"&b投递成功，花费 &6"+df.format(price*itemStack.getAmount())+"&6D"));
-                    }else {
-                        self.sendMessage(ChatColor.translateAlternateColorCodes('&',"&4你没有足够的钱！"));
-                        return false;
-                    }
-                }else {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',this.null_mainhand));
+            Double totalprice = price * getamount(player.getInventory());
+            if (econ.has(player, totalprice)) {
+                if (realAdd(midv, itemStack)) {
+                    inv_s.addItem(itemStack);
+                    ItemStack[] stacks1 = new ItemStack[itemStack.length];
+                    SQLActions.uploaddata(player, inv_s);
+                    player.getInventory().setStorageContents(stacks1);
+                    player.updateInventory();
+                    econ.withdrawPlayer(player, totalprice);
+                    //等待给某op
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            "&b投递成功，花费 &6" + df.format(totalprice) + "&6D"));
+                } else {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            this.no_slots+getAmount(inv_s)+"/"+inv_s.getSize()));
                     return false;
                 }
+            } else {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b你没有足够的钱！"));
+                return false;
             }
-        }else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&4No exist this player!"));
+        } else {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4No exist!"));
             return false;
         }
         return true;
     }
 
+    private boolean sendsingle(Player player, Player self) {
+        DecimalFormat df = new DecimalFormat("0.00 ");
+        if (SQLActions.playerDataContainsPlayer(player.getUniqueId())) {
+            Inventory inv_s = SQLActions.loaddata(player);
+            ItemStack itemStack = self.getInventory().getItemInMainHand();
+
+            Inventory midv = Bukkit.createInventory(null, slots, player.getName());
+            midv.setStorageContents(inv_s.getStorageContents());
+
+            if (itemStack != null && !itemStack.getType().equals(Material.AIR)) {
+                ItemStack midit = new ItemStack(itemStack);
+                if (econ.has(self, price * itemStack.getAmount())) {
+                    if (realadd(midv, midit)) {
+                        inv_s.addItem(itemStack);
+                        SQLActions.uploaddata(player, inv_s);
+                        self.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+                        self.updateInventory();
+                        econ.withdrawPlayer(self, price * itemStack.getAmount());
+                        self.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                "&b投递成功，花费 &6" +
+                                        df.format(price * itemStack.getAmount()) + "&6D"));
+                    } else {
+                        self.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                this.no_slots+getAmount(inv_s)+"/"+inv_s.getSize()));
+                    }
+                } else {
+                    self.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            "&4你没有足够的钱！"));
+                }
+            } else {
+                self.sendMessage(ChatColor.translateAlternateColorCodes('&', this.null_mainhand));
+                return false;
+            }
+        } else {
+            self.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4No exist this player!"));
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean realAdd(Inventory inventory, ItemStack[] itemStacks){
+
+        for (int i = 0; i < itemStacks.length; i++) {
+            ItemStack it = itemStacks[i];
+            if (it == null){
+                itemStacks[i] = new ItemStack(Material.AIR);
+            }
+        }
+
+        Map<Integer, ItemStack> leftover = inventory.addItem(itemStacks);
+        return leftover.isEmpty();
+    }
+
+
+    private boolean realadd(Inventory inventory, ItemStack itemStack){
+
+        Map<Integer, ItemStack> leftover = inventory.addItem(itemStack);
+
+        return leftover.isEmpty();
+    }
 
 }
